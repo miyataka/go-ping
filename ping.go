9e4e162693c96c60007941b49bf85b2e66b21f70
Takayuki Miyahara <voyager.3taka28@gmail.com>
2022-10-15 03:17:19 +0900

    fix: validation checksum bug

--- ping.go
+++ ping.go
@@ -44,14 +44,14 @@ func DoPing(IPv4OrHostname string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("success %d bytes read\n", n)
 
-	ipv4Bytes := buf[:n+1]
+	ipv4Bytes := buf[:n]
 	_, body, err := parseIPv4Packet(ipv4Bytes)
 	if err != nil {
 		log.Fatal(err)
 	}
 	icmpPacket := parseICMPPacket(body)
+	fmt.Printf("success %d bytes read and icmp packet size is %d\n", n, len(body))
 
 	if validateChecksum(icmpPacket) {
 		fmt.Printf("received. Type: %x, Code: %x: Checksum: %x, Identifier: %x, SequenceNumber: %x, Data: %x",
@@ -81,6 +81,8 @@ func parseICMPPacket(b []byte) ICMPPacket {
 	}
 }
 
+var fixedPingData = []byte{0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37}
+
 func NewPingICMPPacket() ICMPPacket {
 	pp := ICMPPacket{
 		Type:           []byte{0x08},
@@ -88,7 +90,7 @@ func NewPingICMPPacket() ICMPPacket {
 		Checksum:       []byte{0x00, 0x00},
 		Identifier:     []byte{0x00, 0x00},
 		SequenceNumber: []byte{0x00, 0x00},
-		Data:           []byte{},
+		Data:           fixedPingData,
 	}
 
 	iPkt := ICMPPacket(pp)
@@ -123,7 +125,7 @@ func checksum(b []byte) []byte {
 			sum += uint(binary.BigEndian.Uint16(b[i:]))
 		}
 	}
-	val := sum ^ 0xffff
+	val := sum + (sum >> 16) ^ 0xffff
 	cs := make([]byte, 2)
 	binary.BigEndian.PutUint16(cs, uint16(val))
 	return cs
@@ -176,6 +178,7 @@ func validateChecksum(packet ICMPPacket) bool {
 		Checksum:       []byte{0x00, 0x00},
 		Identifier:     packet.Identifier,
 		SequenceNumber: packet.SequenceNumber,
+		Data:           packet.Data,
 	}
 	b, _ := Marshal(p)
 	cs2 := checksum(b)
